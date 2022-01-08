1ec35d676ba78ffc4250c004433f37116134a7f9
Evgeny Ustinkin <evgeny@ustinkin.com>
2022-01-08 16:30:07 +0300

    :building_construction: project skeleton

--- main.go
+++ main.go
@@ -2,3 +2,42 @@ package main
 
 func main() {
 }
+
+//Write a function `testValidity` that takes the string as an input,
+//and returns boolean flag `true` if the given string complies with
+//the format, or `false` if the string does not comply
+func testValidity(s string) bool {
+	return false
+}
+
+//Write a function `averageNumber` that takes the string,
+//and returns the average number from all the numbers
+func averageNumber(s string) uint16 {
+	return 0
+}
+
+//Write a function `wholeStory` that takes the string,
+//and returns a text that is composed from all the text
+//words separated by spaces,
+//e.g. `story` called for the string `1-hello-2-world`
+//should return text: `"hello world"
+func wholeStory(s string) string {
+	return ""
+}
+
+//Write a function `storyStats` that returns four things:
+// the shortest word
+// the longest word
+// the average word length
+// the list (or empty list) of all words from the story that
+// have the length the same as the average length rounded up and down.
+func storyStats(str string) (*ShortStoryDto, error) {
+	return nil, nil
+}
+
+type ShortStoryDto struct {
+	ShortestWord      string
+	LongestWord       string
+	AverageWordLength uint16
+	AverageWords      []string
+}
---
+++ main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestTestValidity(t *testing.T) {
+
+}
+
+func TestAverageNumber(t *testing.T) {
+
+}
+
+func TestWholeStory(t *testing.T) {
+
+}
+
+func TestStoryStat(t *testing.T) {
+
+}
